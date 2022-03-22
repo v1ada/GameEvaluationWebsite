8609a190b8f75116abc41b05fa1ee21b39dfee44
v1ada-dell <v1ada1994@163.com>
2022-03-22 14:39:09 +0800

    游戏信息JSON数据 和 游戏信息模型修改

--- server/models/GameInfo.js
+++ server/models/GameInfo.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 
 const gameInfoSchema = new mongoose.Schema({
-  game_cover: { type: String },
+  game_id: { type: String },
+  game_logo: { type: String },
   game_name: { type: String },
   origin_name: { type: String },
-  game_date: { type: String },
   platform: [{ type: String }],
-  game_gene: [{ type: String }],
+  game_type: [{ type: String }],
+  game_cover: { type: String },
+  game_date: { type: String },
   game_developer: { type: String },
   game_publisher: { type: String },
   game_img_list: [{ url: String }],

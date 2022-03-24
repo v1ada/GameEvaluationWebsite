74f510ebdaedba53899da1204cbc8b6cc0467dbe
v1ada-dell <v1ada1994@163.com>
2022-03-24 18:23:46 +0800

    Vuex 的 getter mutaion action 使用测试

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -7,13 +7,43 @@ const store = new Vuex.Store({
   state() {
     return {
       name: '张三',
-      number: 0,
+      age: 0,
+      from: 'china',
       list: [
-        { id: 1, name: '111' },
-        { id: 2, name: '222' },
-        { id: 3, name: '333' },
+        { id: 1, text: '111', done: true },
+        { id: 2, text: '222', done: false },
+        { id: 3, text: '333', done: true },
       ],
     };
+  },
+  getters: {
+    listDone(state) {
+      return state.list.filter((item) => item.done);
+    },
+    // 可以接受第二个参数，为其他getter
+    listDoneCount(state, getters) {
+      return getters.listDone.length;
+    },
+    // 带参数
+    listFindById(state) {
+      return (id) => {
+        return state.list.find((item) => item.id === id);
+      };
+    },
+  },
+  mutations: {
+    ageGrow(state) {
+      state.age++;
+    },
+    // 带载荷
+    ageGrowBy(state, payload) {
+      state.age += payload.num;
+    },
+  },
+  actions: {
+    ageGrow(context) {
+      context.commit('ageGrow');
+    },
   },
 });
 

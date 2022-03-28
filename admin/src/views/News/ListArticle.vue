316ecff85177fadd9e430746272239c59c7f3d39
v1ada-dell <v1ada1994@163.com>
2022-03-28 14:55:53 +0800

    admin文章列表分页

--- admin/src/views/News/ListArticle.vue
+++ admin/src/views/News/ListArticle.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="ListArticle-container">
-    ListArticle
     <el-table :data="articlesData">
-      <el-table-column type="index" :index="1" label="序号" />
+      <el-table-column type="index" :index="index" label="序号" />
       <el-table-column prop="_id" label="ID" />
       <el-table-column prop="title" label="标题" />
       <el-table-column prop="cover" label="封面">
@@ -10,6 +9,7 @@
           <img :src="scope.row.cover" style="height: 3em" />
         </template>
       </el-table-column>
+      <el-table-column prop="publishTime" label="发布时间" />
       <el-table-column label="操作" width="150">
         <template slot-scope="scope">
           <el-button
@@ -23,6 +23,15 @@
         </template>
       </el-table-column>
     </el-table>
+    <!-- 分页 -->
+    <div class="paging">
+      <el-pagination
+        layout="prev, pager, next, jumper, ->, total"
+        :total="articlesTotal"
+        background
+        @current-change="currentChange"
+      />
+    </div>
   </div>
 </template>
 
@@ -32,17 +41,35 @@ export default {
   data() {
     return {
       articlesData: [],
+      articlesTotal: 0,
+      articlesListPage: 1,
     };
+  },
+  computed: {
+    index() {
+      return (this.articlesListPage - 1) * 10 + 1;
+    },
   },
   methods: {
     // 获取所有文章数据
     async getArticlesData() {
       try {
-        const { data } = await this.$http.get('/rest/articles');
+        const { data } = await this.$http.get(
+          `/rest/articles?page=${this.articlesListPage}&sort=pubTimeDesc`
+        );
         this.articlesData = data.result;
+        this.articlesTotal = data.total;
       } catch (err) {
         console.log(err);
       }
+    },
+    // 分页变化事件
+    currentChange(currentPage) {
+      this.articlesListPage = currentPage;
+      // 获取分页数据
+      this.getArticlesData({ page: this.articlesListPage });
+      // 网页偏移到顶部
+      document.documentElement.scrollTop = 0;
     },
     // 删除文章
     removeArticle(row) {

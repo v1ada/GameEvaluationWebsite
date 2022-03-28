272ae7188a63875e26bb48da79ccece387096e2c
v1ada-dell <v1ada1994@163.com>
2022-03-28 09:50:08 +0800

    修改页面title

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -2,4 +2,10 @@ module.exports = {
   devServer: {
     disableHostCheck: true,
   },
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = '游戏评价网站管理后台';
+      return args;
+    });
+  },
 };
--- web/vue.config.js
+++ web/vue.config.js
@@ -2,4 +2,10 @@ module.exports = {
   devServer: {
     port: 8081,
   },
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = '游戏评价网站';
+      return args;
+    });
+  },
 };

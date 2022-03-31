e9b2bd5aae4b2d3652f22a905f5e960955d20448
v1ada-dell <v1ada1994@163.com>
2022-03-31 12:01:39 +0800

    登录状态验证添加注释

--- web/src/App.vue
+++ web/src/App.vue
@@ -24,9 +24,6 @@
             <el-menu-item index="#">个人资料</el-menu-item>
             <el-menu-item index="#" @click="logout">注销</el-menu-item>
           </el-submenu>
-          <!-- <el-menu-item index="#" v-if="this.loginState" class="login-item">
-            {{ this.userData.username }}
-          </el-menu-item> -->
         </el-menu>
       </el-header>
       <el-main>

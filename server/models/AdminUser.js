751c951feb0b4436cd93e4cb5c6112a9a84d6c6d
v1ada-dell <v1ada1994@163.com>
2022-04-01 18:08:01 +0800

    配置 @ 的路径提示

--- server/models/AdminUser.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const schema = new mongoose.Schema({
-  username: { type: String },
-  password: { type: String },
-});
-
-module.exports = mongoose.model('AdminUser', schema);
--- web/vue.config.js
+++ web/vue.config.js
@@ -1,11 +1,17 @@
+const path = require('path');
+const resolve = (dir) => path.join(__dirname, dir);
+
 module.exports = {
   devServer: {
     port: 8081,
   },
+  // baseUrl: './',
+  runtimeCompiler: true,
   chainWebpack: (config) => {
     config.plugin('html').tap((args) => {
       args[0].title = '游戏评价网站';
       return args;
     });
+    config.resolve.alias.set('@', resolve('src'));
   },
 };

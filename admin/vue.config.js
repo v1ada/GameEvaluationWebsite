ba5a7d1816b8083f4cfc12b7f5561289a9e63c75
v1ada-dell <v1ada1994@163.com>
2022-05-24 15:11:06 +0800

    设置请求的baseURL为环境变量

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -1,8 +1,10 @@
 module.exports = {
+  // 设置 开发环境 服务器端口
   devServer: {
     disableHostCheck: true,
     port: 8080,
   },
+  // 设置浏览器 title
   chainWebpack: (config) => {
     config.plugin('html').tap((args) => {
       args[0].title = '游戏评价网站管理后台';
--- web/vue.config.js
+++ web/vue.config.js
@@ -2,16 +2,18 @@ const path = require('path');
 const resolve = (dir) => path.join(__dirname, dir);
 
 module.exports = {
+  // 设置 开发环境 服务器端口
   devServer: {
     port: 8081,
   },
-  // baseUrl: './',
   runtimeCompiler: true,
+  // 设置浏览器 title
   chainWebpack: (config) => {
     config.plugin('html').tap((args) => {
       args[0].title = '游戏评价网站';
       return args;
     });
+    // 设置 @ 为根目录
     config.resolve.alias.set('@', resolve('src'));
   },
 };
